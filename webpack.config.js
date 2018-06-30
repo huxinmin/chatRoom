a5c7943d648d16d6962a90289a113f3dd2d61c41
huxinmin <744019705@qq.com>
2018-06-30 17:45:34 +0800

    webpack config finished

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, './client/src/js/index.js'),
+
+  target: 'web',
+
+  watch: true,
+  watchOptions: {
+  	ignored: /node_modules/,
+  	aggregateTimeout: 300,
+  	poll: 1000
+	},
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist')
+  },
+
+  module:{
+		rules:[
+			{
+				test:/.js$/,
+				use:[ {loader:'babel-loader'}, {loader:'eslint-loader'} ],
+				exclude: /node_modules/
+			},{
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      }
+    ]
+	},
+
+	externals: {
+  	jquery: 'jQuery'
+	},
+
+	plugins: [
+    new HtmlWebpackPlugin({
+    	filename:'index.html',
+      template: './client/src/index.html'
+    }),
+    new ExtractTextPlugin("bundle.css"),
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(['./client/dist'], {root: __dirname,exclude: [ 'assets']})
+  ]
+};
