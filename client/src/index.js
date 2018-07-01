098b74653f3cedc2b4e53f7bf0a4cd4e17956d13
huxinmin <744019705@qq.com>
2018-07-01 13:52:46 +0800

    add fireworks

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import page from 'page';
 import $ from 'jquery';
 import Login from './routes/login';
 import './common.less';
+import './utils/fireworks.js';
 
 const app = $('#app');
 page.base('/chatRoom');
