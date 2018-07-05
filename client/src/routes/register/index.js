5a13c052cc9958a201535e5773238ca5856e1271
huxinmin <744019705@qq.com>
2018-07-05 22:42:31 +0800

    add avater cropper

--- client/src/routes/register/index.js
+++ client/src/routes/register/index.js
@@ -1,8 +1,15 @@
 import "./index.less";
 import regEvent from"./events";
 
-var source = "<div class={{loginClass}}>"
-+"<label for='avaterInput'><input id='avaterInput' type='file' style='display:none;'>选择头像</label>"
+var source = "<div class='register-wrapper' style='width:200px'>"
++  "<label for='avaterInput' style='display:block; overflow:hidden;width:100px;height:100px;border-radius:50%;position:relative;'>"
++ "<img style='width:100%' src='/assets/images/avater.jpg' class='register-avater'><input id='avaterInput' type='file' style='display:none;'>"
++ "<span style='position:absolute;left:10px;top:30px;'>选择头像</span></label>"
++"<form class='register-form'>"
++  "<div class='register-form-group'><input class='register-name' type='text' placeholder='请输入用户名'></div>"
++  "<div class='register-form-group'><input class='register-password' type='password' placeholder='请输入密码'></div>"
++  "<div class='register-form-group'><input class='register-cfPassword' type='password' placeholder='请再次输入密码'></div>"
++"</form>"
 +"</div>";
 
 
